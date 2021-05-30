3c37116ad33dc2dc797bec0ae304a90522ad155c
Дмитрий Двойченко <stud0000233461@study.utmn.ru>
2021-05-30 14:28:02 +0500

    начальная структура1

---
+++ GameSpaceArcanoid2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace GameSpaceArcanoid2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BallImg = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BallImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.BallImg);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(531, 418);
+            this.panel1.TabIndex = 0;
+            // 
+            // BallImg
+            // 
+            this.BallImg.Image = ((System.Drawing.Image)(resources.GetObject("BallImg.Image")));
+            this.BallImg.Location = new System.Drawing.Point(223, 170);
+            this.BallImg.Name = "BallImg";
+            this.BallImg.Size = new System.Drawing.Size(38, 36);
+            this.BallImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BallImg.TabIndex = 0;
+            this.BallImg.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 418);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.BallImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox BallImg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GameSpaceArcanoid2/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
+
+
+namespace GameSpaceArcanoid2
+{
+    public partial class Form1 : Form
+    {
+        int o = 1;
+        double sx = 4, sy = 5, bx, by, sin = 1, cos = 0;
+
+        double nap1 = 0, width, height;
+
+        double nap2 = -10;
+
+        float LX, LY;
+        bool BallIsBottom = true;
+        bool IsActiv = true;
+
+        public Form1(int width = 1000, int height = 500)
+        {
+            DoubleBuffered = true;
+            this.Width = width;
+            this.Height = height;
+            new Thread(() => { while (IsActiv) Invalidate(); }).Start();
+            FormClosed += (s, e) => IsActiv = false;
+        }
+
+        
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+
+                Thread.Sleep(100);
+                if (nap1 < 8)
+                {
+                    nap1 += 0.5;
+                    if (nap1 < 0)
+                    {
+                        nap2 = -10 - nap1;
+                    }
+                    else { nap2 = -10 + nap1; }
+                }
+                label1.Text = $"{nap1}";
+                label2.Text = $"{nap2}";
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                Thread.Sleep(100);
+                if (nap1 > -8)
+                {
+                    nap1 -= 0.5;
+                    if (nap1 < 0)
+                    {
+                        nap2 = -10 - nap1;
+                    }
+                    else { nap2 = -10 + nap1; }
+                }
+                label1.Text = $"{nap1}";
+                label2.Text = $"{nap2}";
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                if (BallIsBottom == false)
+                {
+                    BallImg.Top = panel1.Height - BallImg.Height - 30;
+                    BallImg.Left = panel1.Width / 2;
+                    BallIsBottom = true;
+                    sx = nap1;
+                    sy = nap2;
+                }
+            }
+        }
+
+       
+
+
+
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            
+
+            bx = BallImg.Left;
+            by = BallImg.Top;
+
+            var timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += (sender, args) =>
+            {
+                label2.Text = "jo";
+                Invalidate();
+            };
+            timer.Start();
+
+
+            Paint += MoveBall;
+           
+        }
+
+        private void MoveBall(object sender, PaintEventArgs e)
+        {
+            if (BallIsBottom == true)
+            {
+                double radius = (panel1.Height - 120);
+                LX = (float)((panel1.Width / 2) + radius * Math.Cos(cos));
+                LY = (float)((panel1.Height) - radius * Math.Sin(sin));
+                bx += sx;
+                by += sy;
+
+                BallImg.Left = (int)bx;
+                BallImg.Top = (int)by;
+                label1.Text = "jkl";
+                if (BallImg.Top <= 0)
+                {
+                    sy *= -1;
+                }
+                if ((BallImg.Right >= panel1.Width) || (BallImg.Left <= 0))
+                {
+                    sx *= -1;
+                }
+                if (BallImg.Top + BallImg.Height >= panel1.Height)
+                {
+                    BallIsBottom = false;
+                    sx = 0; sy = 0;
+                    by = panel1.Height - BallImg.Height;
+                    bx = panel1.Width / 2;
+                    BallImg.Top = panel1.Height - BallImg.Height;
+                    BallImg.Left = panel1.Width / 2;
+
+                }
+            }
+            width = panel1.Width;
+            height = panel1.Height;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+        public void MoveBall()
+        {
+            
+        }
+
+    }
+}
