07e1b1622d61638c65a1cbfbc36055521743de1e
Дмитрий Двойченко <stud0000233461@study.utmn.ru>
2021-06-25 21:38:17 +0500

    Начинаем рефакторинг

--- GameSpaceArcanoid2/Form1.Designer.cs
+++ GameSpaceArcanoid2/Form1.Designer.cs
@@ -47,9 +47,8 @@ namespace GameSpaceArcanoid2
             this.panel1.Controls.Add(this.BallImg);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(708, 514);
+            this.panel1.Size = new System.Drawing.Size(531, 418);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -58,51 +57,48 @@ namespace GameSpaceArcanoid2
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Location = new System.Drawing.Point(9, 7);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 32);
+            this.label3.Size = new System.Drawing.Size(93, 26);
             this.label3.TabIndex = 3;
             this.label3.Text = "X   X   X";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(207, 113);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(155, 92);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(203, 71);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(152, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
             // BallImg
             // 
             this.BallImg.Image = ((System.Drawing.Image)(resources.GetObject("BallImg.Image")));
-            this.BallImg.Location = new System.Drawing.Point(301, 384);
-            this.BallImg.Margin = new System.Windows.Forms.Padding(4);
+            this.BallImg.Location = new System.Drawing.Point(235, 318);
             this.BallImg.Name = "BallImg";
-            this.BallImg.Size = new System.Drawing.Size(51, 44);
+            this.BallImg.Size = new System.Drawing.Size(29, 30);
             this.BallImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.BallImg.TabIndex = 0;
             this.BallImg.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(708, 514);
+            this.ClientSize = new System.Drawing.Size(531, 418);
             this.Controls.Add(this.panel1);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
