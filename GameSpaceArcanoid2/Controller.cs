3e9e02a5e39c525a5d98a72a6eb76f61c10b2496
Дмитрий Двойченко <stud0000233461@study.utmn.ru>
2021-06-25 22:42:44 +0500

    Вынес контроллер в отдельный класс. Для работы с нажатием кнопок создал словарь с анонимными делегатами Action и значением кнопок

--- GameSpaceArcanoid2/Controller.cs
+++ GameSpaceArcanoid2/Controller.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GameSpaceArcanoid2
 {
-    class Controller
+    public class Controller
     {
+        private Control _control;
+        private Ball _ball;
+
+        private Dictionary<Keys, Action> PressKey;
+
+        public Controller(Control control)
+        {
+            _control = control;
+            _ball = new Ball(_control);
+            PressKey = new Dictionary<Keys, Action>()
+            {
+                { Keys.Right, () => _ball.DirectionX += 1 },
+                { Keys.Left, () => _ball.DirectionX -= 1 },
+                { Keys.Space, () =>
+                    {
+                        if (_ball.IsFlying)
+                            _ball.StopFlight();
+                        else
+                            _ball.IsFlying = true;
+                    }
+                }
+            };
+            _control.KeyDown += (s, e) =>
+            {
+                if (PressKey.ContainsKey(e.KeyCode))
+                    PressKey[e.KeyCode].Invoke();
+            };
+        }
+
+        public void Start()
+        {
+            var timer = new Timer();
+            timer.Interval = 20;
+            timer.Tick += (s, e) => Update();
+            timer.Tick += (s, e) => _control.Invalidate();
+            timer.Start();
+        }
+
+        private void Update()
+        {
+            if (_ball.IsFlying)
+                _ball.Move();
+        }
     }
 }
